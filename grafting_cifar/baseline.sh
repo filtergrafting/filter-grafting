61616dcfe671cdb3606882590bbf85b1243285b2
fxmeng <60565778+fxmeng@users.noreply.github.com>
2021-01-14 17:54:04 +0800

    Delete grafting_cifar directory

--- grafting_cifar/baseline.sh
+++
@@ -1,4 +0,0 @@
-mkdir -pv checkpoint/baseline_cifar10_resnet32;
-CUDA_VISIBLE_DEVICES=0 nohup python baseline.py --s checkpoint/baseline_cifar10_resnet32 --cifar 10  --model resnet32 >checkpoint/baseline_cifar10_resnet32/1.log &
-mkdir -pv checkpoint/baseline_cifar10_mobilenetv2;
-CUDA_VISIBLE_DEVICES=1 nohup python baseline.py --s checkpoint/baseline_cifar10_mobilenetv2 --cifar 10  --model mobilenetv2 >checkpoint/baseline_cifar10_mobilenetv2/1.log &
--- grafting_cifar/grafting.sh
+++
@@ -1,6 +0,0 @@
-mkdir -pv checkpoint/grafting_cifar10_resnet32;
-CUDA_VISIBLE_DEVICES=0 nohup python grafting.py --s checkpoint/grafting_cifar10_resnet32 --cifar 10  --model resnet32 --num 2 --i 1 >checkpoint/grafting_cifar10_resnet32/1.log &
-CUDA_VISIBLE_DEVICES=1 nohup python grafting.py --s checkpoint/grafting_cifar10_resnet32 --cifar 10  --model resnet32 --num 2 --i 2 >checkpoint/grafting_cifar10_resnet32/2.log &
-mkdir -pv checkpoint/grafting_cifar10_mobilenetv2;
-CUDA_VISIBLE_DEVICES=2 nohup python grafting.py  --s checkpoint/grafting_cifar10_mobilenetv2 --cifar 10  --model mobilenetv2 --num 2 --i 1 >checkpoint/grafting_cifar10_mobilenetv2/1.log &
-CUDA_VISIBLE_DEVICES=3 nohup python grafting.py  --s checkpoint/grafting_cifar10_mobilenetv2 --cifar 10  --model mobilenetv2 --num 2 --i 2 >checkpoint/grafting_cifar10_mobilenetv2/2.log &
